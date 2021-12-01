622aa97eecefb886e23b6bec0e0eeb797af0d59c
Lucas Oliveira <lucasoliveira226@gmail.com>
2021-12-01 16:02:44 -0300

    Requisito 3 aprovado, porém ainda incompleto

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Testes do componente FavoritePokemons', () => {
+  it('Testa se é exibido na tela a mensagem "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavorite = screen.getByText(/no favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
