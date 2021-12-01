e59e42e40620bbbabcbd91eaafacdd8694a42f7a
Lucas Oliveira <lucasoliveira226@gmail.com>
2021-12-01 16:53:01 -0300

    Requisito 4 completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Testes do componente NotFound', () => {
+  it('Testa se a página contem um h2 com o texto...', () => {
+    renderWithRouter(<NotFound />);
+    const requestNotFound = screen.getByRole('heading',
+      { name: /Page requested not found/i },
+      { level: 2 });
+    expect(requestNotFound).toBeInTheDocument();
+  });
+
+  it('Testa se a página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const requestNotFoundImg = screen.getByRole('img',
+      { name: /pikachu crying because the page requested was not found/i });
+
+    expect(requestNotFoundImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(requestNotFoundImg).toBeInTheDocument();
+  });
+});
